93c8a96475c57c0ff0368a54c93e959fab6ff92a
Guillaume Duhan <myhappyagency@gmail.com>
2020-11-28 18:48:17 +0000

    Update Login.vue

--- components/auth/Login.vue
+++ components/auth/Login.vue
@@ -61,7 +61,7 @@ export default {
             this.isLoading = false
           })
         } catch (err) {
-          console.log(err)
+          this.displayNotification('Cannot login. Please try again.', 'danger')
         }
       }
     },
